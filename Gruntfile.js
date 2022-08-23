5cbf56ac4ddf41313c58bfd1a893e009fdd0e70c
edwin andres legro agudelo <maxedwwin@gmail.com>
2022-08-22 23:51:51 -0500

    init

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+"use strict";
+
+module.exports = function (grunt) {
+	require("time-grunt")(grunt);
+	require('load-grunt-tasks')(grunt);
+
+	grunt.loadNpmTasks("grunt-concat-css");
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		concat: {
+			resources: {
+				src: [
+					"node_modules/jquery/dist/jquery.js",
+					"node_modules/jquery-mobile/dist/jquery.mobile.js",
+					"node_modules/underscore/underscore-umd.js",
+                    "node_modules/backbone/backbone.js"
+				],
+				dest: "src/cp/build.resources.js"
+			}
+		},
+		babel: {
+			options: {
+			  	presets: ['@babel/preset-env']
+			},
+			dist: {
+			  	files: [{
+					expand: true,
+					cwd: "src/js/",
+					src: ['**/*.js'],
+					dest: 'www/static/js'
+				}]
+			}
+		},
+		concat_css: {
+			options: {
+			},
+			all: {
+				src: [
+					"node_modules/jquery-mobile/dist/jquery.mobile.structure.css",
+					"node_modules/jquery-mobile/dist/jquery.mobile.icons.css",
+					"src/css/theme.css"
+				],
+				dest: "www/static/css/build.styles.css",
+			}
+		},
+		uglify: {
+			dist: {
+                files: [{
+					expand: true,
+					cwd: "src/cp/",
+					src: ['**/*.js'],
+					dest: 'www/static/js'
+				}]
+            }
+		},
+		watch: {
+            files: [
+                '<%= uglify.dist.files %>',
+				'<%= babel.dist.files %>'
+            ],
+            tasks: [
+                'uglify',
+				'babel'
+            ]
+        },
+		copy: {
+			main: {
+				files: [
+					{
+						expand: true, 
+						cwd: "node_modules/underscore/",
+						src: "underscore-umd.js.map", 
+						dest: "www/static/js/", 
+						filter: "isFile"
+					},
+					{
+						expand: true, 
+						cwd: "src/images/",
+						src: ['**'], 
+						dest: "www/static/img/"
+					},
+					{
+						expand: true, 
+						cwd: "src/fonts/",
+						src: ['**'], 
+						dest: "www/static/fonts/"
+					}
+				]
+			}
+		}
+	});
+
+	grunt.registerTask("default", [
+		"concat",
+		"babel", 
+		"concat_css",
+		"uglify"
+	]);
+};
---
+++ src/js/dash.index.js
@@ -0,0 +1,100 @@
+"use strict";
+
+let ConnectionDb = (() => {
+	let db;
+
+	let Connect = (Callback = void 0) => {
+	}
+
+	let Errores = (e) => {
+		console.log('Error: '+e.target.error)
+	}
+
+	let SuccessDb = (tx) => {
+	}
+
+	let searchData = (e) => {
+	}
+
+	let report = (msj)=> {
+		$('#msjPopup').html(msj)
+		$( "#popupBasic" ).popup( "option", "arrow" )
+	}
+
+	return {
+		init: () => {
+			Connect(SuccessDb)
+			return true;
+		}
+	}
+})();
+
+var App = {
+	Models: {},
+	Collections: {},
+	Routers: {}
+};
+
+App.Routers = App.Routers || {}
+
+var DashRouter = Backbone.Router.extend( {
+	initialize: () =>  {
+		Backbone.history.start()
+	},
+	routes: {
+		'': 'homePage',
+		'login': 'loginAuth',
+		'registro': 'registerAuth',
+		'start':'startApp'
+	},
+	homePage: () => {
+		console.log('homePage')
+		$.mobile.changePage( "#pageIndex" , { reverse: false, changeHash: false } );
+		$.mobile.loading( "hide" );
+	},
+
+	loginAuth: () => {
+		console.log('loginAuth')
+		$.mobile.changePage( "#pageLogin" , { reverse: false, changeHash: false } );
+	},
+
+	registerAuth: () => {
+		console.log('registerAuth')
+		$.mobile.changePage( "#pageRegistro" , { reverse: false, changeHash: false } );
+	},
+
+	startApp: () =>  {
+		alert('startApp')
+	}
+})
+
+
+const DashIndex = () => {
+
+	let onDeviceReady = () => {
+		$.mobile.loading( "show" );
+		let $el = document.getElementById('deviceready')
+		receivedEvent($el)
+	}
+
+	let receivedEvent = ($el) => {
+        console.log("Inicializa la aplicación ready ok")
+		$.mobile.linkBindingEnabled = false;
+		$.mobile.hashListeningEnabled = false;
+		App.Routers.router = new DashRouter()
+	}
+
+	let initialize = () => {
+		//document.addEventListener("deviceready", onDeviceReady, false)
+		window.addEventListener('deviceready', onDeviceReady, false)
+	}
+
+	return {
+		"initialize":initialize,
+		"onDeviceReady": onDeviceReady,
+		"receivedEvent": receivedEvent
+	}
+}
+
+let dashIndex = DashIndex()
+dashIndex.initialize()
